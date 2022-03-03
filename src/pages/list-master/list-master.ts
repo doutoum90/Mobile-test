481c287761a3aef24830d6d44339164f0ad43e7d
Mahamat <annour.mahamat-ext@kering.com>
2022-03-03 17:41:47 +0100

    first reading

--- src/pages/list-master/list-master.ts
+++ src/pages/list-master/list-master.ts
@@ -1,38 +1,41 @@
-import { Component } from '@angular/core';
-import { IonicPage, ModalController, NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, ModalController, NavController } from "ionic-angular";
 
-import { Item } from '../../models/item';
-import { Items } from '../../providers';
+import { Item } from "../../models/item";
+import { Items } from "../../providers";
 
 @IonicPage()
 @Component({
-  selector: 'page-list-master',
-  templateUrl: 'list-master.html'
+  selector: "page-list-master",
+  templateUrl: "list-master.html",
 })
 export class ListMasterPage {
   currentItems: Item[];
 
-  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
+  constructor(
+    public navCtrl: NavController,
+    public items: Items,
+    public modalCtrl: ModalController
+  ) {
     this.currentItems = this.items.query();
   }
 
   /**
    * The view loaded, let's query our items for the list
    */
-  ionViewDidLoad() {
-  }
+  ionViewDidLoad() {}
 
   /**
    * Prompt the user to add a new item. This shows our ItemCreatePage in a
    * modal and then adds the new item to our data source if the user created one.
    */
   addItem() {
-    let addModal = this.modalCtrl.create('ItemCreatePage');
-    addModal.onDidDismiss(item => {
+    let addModal = this.modalCtrl.create("ItemCreatePage");
+    addModal.onDidDismiss((item) => {
       if (item) {
         this.items.add(item);
       }
-    })
+    });
     addModal.present();
   }
 
@@ -47,8 +50,8 @@ export class ListMasterPage {
    * Navigate to the detail page for this item.
    */
   openItem(item: Item) {
-    this.navCtrl.push('ItemDetailPage', {
-      item: item
+    this.navCtrl.push("ItemDetailPage", {
+      item: item,
     });
   }
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,53 +1,57 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import { Component, OnInit } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { IonicPage, NavController, ToastController } from "ionic-angular";
 
-import { User } from '../../providers';
-import { MainPage } from '../';
+import { User } from "../../providers";
+import { MainPage } from "../";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html'
+  selector: "page-signup",
+  templateUrl: "signup.html",
 })
-export class SignupPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
-    name: 'Test Human',
-    email: 'test@example.com',
-    password: 'test'
-  };
-
-  // Our translated text strings
+export class SignupPage implements OnInit {
+  signupformGroup: FormGroup;
   private signupErrorString: string;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public user: User,
     public toastCtrl: ToastController,
-    public translateService: TranslateService) {
-
-    this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
+    public translateService: TranslateService
+  ) {
+    this.translateService.get("SIGNUP_ERROR").subscribe((value) => {
       this.signupErrorString = value;
-    })
+    });
+  }
+
+  ngOnInit() {
+    this.signupformGroup = new FormGroup({
+      name: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl(),
+      username: new FormControl(),
+      passwordConfirm: new FormControl(),
+    });
   }
 
   doSignup() {
     // Attempt to login in through our User service
-    this.user.signup(this.account).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
-
-      this.navCtrl.push(MainPage);
-
-      // Unable to sign up
-      let toast = this.toastCtrl.create({
-        message: this.signupErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-    });
+    this.user.signup(this.signupformGroup.value).subscribe(
+      (resp) => {
+        this.navCtrl.push(MainPage);
+      },
+      (err) => {
+        this.navCtrl.push(MainPage);
+        // Unable to sign up
+        let toast = this.toastCtrl.create({
+          message: this.signupErrorString,
+          duration: 3000,
+          position: "top",
+        });
+        toast.present();
+      }
+    );
   }
 }
